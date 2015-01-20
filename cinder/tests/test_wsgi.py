4c3944088f62f431698da230a93d15b722b041b6
Sam Morrison <sorrison@gmail.com>
2015-01-20 17:55:33 +1100

    Revert "Remove import of private _lazy module"
    This reverts commit 0df51d62cae848db3c9a1a0a5360a3e492739158.
    
    Change-Id: I15c569fe915175f263e2c105e5eeea7a78712274

--- cinder/tests/test_wsgi.py
+++ cinder/tests/test_wsgi.py
@@ -22,12 +22,13 @@ import urllib2
 
 import mock
 from oslo.config import cfg
-from oslo_i18n import fixture as i18n_fixture
+from oslo.i18n import _lazy
 import testtools
 import webob
 import webob.dec
 
 from cinder import exception
+from cinder import i18n
 from cinder.i18n import _
 from cinder import test
 import cinder.wsgi
@@ -219,7 +220,12 @@ class ExceptionTest(test.TestCase):
 
     def setUp(self):
         super(ExceptionTest, self).setUp()
-        self.useFixture(i18n_fixture.ToggleLazy(True))
+        back_use_lazy = _lazy.USE_LAZY
+        i18n.enable_lazy()
+        self.addCleanup(self._restore_use_lazy, back_use_lazy)
+
+    def _restore_use_lazy(self, back_use_lazy):
+        _lazy.USE_LAZY = back_use_lazy
 
     def _wsgi_app(self, inner_app):
         # NOTE(luisg): In order to test localization, we need to
