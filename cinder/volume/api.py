14b2300dbe4fa83d6e957811f18f10f0afdf7bed
Sam Morrison <sorrison@gmail.com>
2015-03-16 09:33:54 +1100

    Only use the start of the AZ for type conversion
    Change-Id: I848b0321397e199ba7bbbcc70a0b152e2a79a172

--- cinder/volume/api.py
+++ cinder/volume/api.py
@@ -189,8 +189,9 @@ class API(base.Base):
 
         if CONF.az_as_volume_type and not volume_type:
             try:
+                az_start = availability_zone.split('-')[0]
                 volume_type = volume_types.get_volume_type_by_name(
-                    context, availability_zone)
+                    context, az_start)
             except exception.VolumeTypeNotFoundByName:
                 msg = "Volume type for AZ %s not found" % availability_zone
                 LOG.exception(msg)
