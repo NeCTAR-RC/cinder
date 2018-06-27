0439889859c79e00b73d1fd1f2685676add9cb3f
TommyLike <tommylikehu@gmail.com>
2018-06-27 10:55:48 +0800

    Handling type's multiattach extra specs in API task flow
    This patch fixs two issues related to multiattach extra specs:
    
    1. Volume's multiattach attribute will always be False if it's
       created by a multiattach type while type is specified in source
       volume, snapshot, image metadata or config file, related patch [1].
    2. 'MULTIATTACH_POLICY' will not be validated if multiattach flag is
       specified in the volume type that from source volume, snapshot,
       image metadata or config file [2].
    
    [1]: a32e24ff3e2c3083b42dd3654355f8c101363147
    [2]: ff30971d1f7b1f4febdef9358a999017bea2296d
    Closes-Bug: #1770671
    
    Conflicts:
        cinder/tests/unit/volume/flows/test_create_volume_flow.py
        cinder/volume/flows/api/create_volume.py
    
    Most conflicts caused by the az-in-volumetype work done in
    I4e6aa7af707bd063e7edf2b0bf28e3071ad5c67a and
    Ibe937d5a97d685f5d9c3f8c03c5c384bb02a6942.
    
    Change-Id: Ibec0656d9ead726ca12e62091fda74c04ec99eda
    (cherry picked from commit 23f26555b3d4d1a089d09cd2335c39a73997c55e)

--- cinder/volume/api.py
+++ cinder/volume/api.py
@@ -313,9 +313,6 @@ class API(base.Base):
 
         utils.check_metadata_properties(metadata)
 
-        if (volume_type and self._is_multiattach(volume_type)) or multiattach:
-            context.authorize(vol_policy.MULTIATTACH_POLICY)
-
         create_what = {
             'context': context,
             'raw_size': size,
@@ -332,7 +329,7 @@ class API(base.Base):
             'optional_args': {'is_quota_committed': False},
             'consistencygroup': consistencygroup,
             'cgsnapshot': cgsnapshot,
-            'multiattach': multiattach,
+            'raw_multiattach': multiattach,
             'group': group,
             'group_snapshot': group_snapshot,
             'source_group': source_group,
@@ -373,8 +370,6 @@ class API(base.Base):
                 # Refresh the object here, otherwise things ain't right
                 vref = objects.Volume.get_by_id(
                     context, vref['id'])
-                vref.multiattach = (self._is_multiattach(volume_type) or
-                                    multiattach)
                 vref.save()
                 LOG.info("Create volume request issued successfully.",
                          resource=vref)
