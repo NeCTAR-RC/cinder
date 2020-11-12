5cf77bb68f6ce0da85304870b87be44bb657ceae
Sam Morrison <sorrison@gmail.com>
2020-11-12 16:50:23 +1100

    Don't set volume type when snapshot or volume source specified
    This fixes a bug in previous commit, if omitted it will
    use the source volume as the volume_type which is what
    we want.
    
    Change-Id: Idfd1fe7528447c73c93e75827e84e83e35e1910f

--- cinder/volume/api.py
+++ cinder/volume/api.py
@@ -289,8 +289,11 @@ class API(base.Base):
                 raise exception.InvalidInput(reason=msg)
 
         if CONF.az_as_volume_type:
-            volume_type = self._get_volume_type_for_az(context, volume_type,
-                                                       availability_zone)
+            if snapshot or source_volume:
+                volume_type = None
+            else:
+                volume_type = self._get_volume_type_for_az(
+                    context, volume_type, availability_zone)
 
         # ensure we pass the volume_type provisioning filter on size
         volume_types.provision_filter_on_size(context, volume_type, size)
