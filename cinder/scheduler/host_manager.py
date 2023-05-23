d6ed0221f6aeab2f7b2adb72c9a222fd4a750c7c
Dylan Mcculloch <dmc@unimelb.edu.au>
2023-05-23 11:50:01 +1000

    Nectarism: Use same host for backup service when available
    While we leave backup_use_same_host = false (default), it
    can still be preferred to use the same host when available,
    but not enforced as a hard requirement.
    
    Setting backup_use_same_host to true is not ideal as it would
    enforce that setting across the federation, and would affect
    HA setups. e.g. causing failures when a backup host is down
    but another host could have served the request.
    
    Change-Id: I6e6e1536f50c61c61bf78e84eeaa36f3d0370991

--- cinder/scheduler/host_manager.py
+++ cinder/scheduler/host_manager.py
@@ -1026,12 +1026,23 @@ class HostManager(object):
         return services[0] if services else None
 
     def _get_available_backup_service_host(self, host, az, driver=None) -> str:
-        """Return an appropriate backup service host."""
+        """Return an appropriate backup service host.
+
+        If a host is specified and it is enabled then return that backup
+        host.
+        If backup_use_same_host is true and a host is specified then only
+        return the host if it is enabled, otherwise raise an exception.
+        If no host is specified then get any available backup host in az.
+        """
         backup_host = None
-        if not host or not CONF.backup_use_same_host:
+        if host:
+            if self._is_backup_service_enabled(az, host):
+                backup_host = host
+            elif not CONF.backup_use_same_host:
+                backup_host = self._get_any_available_backup_service(az,
+                                                                     driver)
+        else:
             backup_host = self._get_any_available_backup_service(az, driver)
-        elif self._is_backup_service_enabled(az, host):
-            backup_host = host
         if not backup_host:
             raise exception.ServiceNotFound(service_id='cinder-backup')
         return backup_host
